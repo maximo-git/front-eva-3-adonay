d9bd5a5645dab843b065acb7fd91252c27bf17ef
Valerie-hubgit <valeria.catalanl22@gmail.com>
2026-06-21 18:56:54 -0400

    se importo empleado, registo

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,9 @@ import Productos from './pages/Productos';
 import ProductoDetalle from './pages/ProductoDetalle';
 import Sedes from './pages/Sedes';
 import SedeDetalle from './pages/SedeDetalle';
+import Empleado from './pages/Empleados';
+import EmpleadoDetalle from './pages/EmpleadoDetalle';
+import Registro from './pages/Registro'; 
 import './App.css';
 
 export default function App() {
@@ -16,14 +19,17 @@ export default function App() {
         <Routes>
           {/* Ruta pública */}
           <Route path="/login" element={<Login />} />
+          <Route path="/registro" element={<Registro />} />
 
           {/* Rutas protegidas — envueltas en Layout que verifica sesión */}
           <Route element={<Layout />}>
-            <Route path="/resumen" element={<Resumen />} />
-            <Route path="/productos" element={<Productos />} />
-            <Route path="/productos/:id" element={<ProductoDetalle />} />
-            <Route path="/sedes" element={<Sedes />} />
-            <Route path="/sedes/:id" element={<SedeDetalle />} />
+          <Route path="/resumen" element={<Resumen />} />
+          <Route path="/productos" element={<Productos />} />
+          <Route path="/productos/:id" element={<ProductoDetalle />} />
+          <Route path="/sedes" element={<Sedes />} />
+          <Route path="/sedes/:id" element={<SedeDetalle />} />
+          <Route path="/empleados" element={<Empleado />} />
+          <Route path="/empleados/:id" element={<EmpleadoDetalle />} />
           </Route>
 
           {/* Redireccionamiento por defecto */}
